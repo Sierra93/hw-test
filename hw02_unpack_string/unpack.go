87dd40e9f282a09523259ba81fd2979acfdf4315
Timoshenko Anton <timoshenko-ai@ntcees.ru>
2025-06-17 19:21:54 +0300

    Реализовал логику.

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,45 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
-var ErrInvalidString = errors.New("invalid string")
+/*
+Unpack Функция распаковывает строку и возвращает повторяющиеся символы.
+*/
+func Unpack(s string) (r string, err error) {
+	if _, err := strconv.Atoi(s); err == nil {
+		return r, errors.New("invalid string")
+	}
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+	var prev rune
+	var escaped bool
+	var b strings.Builder
+
+	for _, char := range s {
+		// Является ли символ числом.
+		if unicode.IsNumber(char) && !escaped {
+			m := int(char - '0')
+			r := strings.Repeat(string(prev), m-1)
+
+			// Пишем в билдер новую строку, состоящую из копий символов, которые перебираем сейчас.
+			b.WriteString(r)
+		} else {
+			// Поддержка экранирования строк.
+			escaped = string(char) == "\\" && string(prev) != "\\"
+
+			// Если пропуск не нужен, то пишем такой символ в билдер.
+			if !escaped {
+				b.WriteRune(char)
+			}
+
+			// Предыдущим становится текущий символ.
+			prev = char
+		}
+	}
+
+	// Возвращаем построенный билдер.
+	return b.String(), err
 }
