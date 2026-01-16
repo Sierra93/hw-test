dc5630594fb1d7696c71fe9888d94e7a85170410
Anton <timoshenko-ai@ntcees.ru>
2026-01-16 19:16:21 +0300

    Revert "Линтер считает, что t не юзаю, но он внутри require есть."
    This reverts commit 1dae1a0da7cdfc0e05c70c7bd4314d2e4d134a9a.

--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestList(t *testing.T) { // nolint
+func TestList(t *testing.T) { //nolint
 	t.Run("empty list", func(t *testing.T) {
 		l := NewList()
 
@@ -50,7 +50,7 @@ func TestList(t *testing.T) { // nolint
 	})
 }
 
-func TestCache_SetAndGet_UpdateValueAndOrder(t *testing.T) { // nolint
+func TestCache_SetAndGet_UpdateValueAndOrder(t *testing.T) { //nolint
 	cache := NewCache(2)
 
 	// Первый вызов - добавляем новый ключ
@@ -91,7 +91,7 @@ func TestCache_SetAndGet_UpdateValueAndOrder(t *testing.T) { // nolint
 	require.Equal(t, "key2", cache.(*lruCache).queue.Back().Value.(*cacheItem).key)
 }
 
-func TestCache_GetRefreshOrder(t *testing.T) { // nolint
+func TestCache_GetRefreshOrder(t *testing.T) { //nolint
 	cache := NewCache(2)
 
 	cache.Set("k1", "v1")
