e5b40c2986ac84c529eafdff456292cf8a1e72cf
Anton <timoshenko-ai@ntcees.ru>
2026-01-17 17:42:08 +0300

    Добавил логику.

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,48 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	outs := make([]Out, len(stages))
+	out := make(Bi)
+	finish := make(chan struct{})
+
+	for i, stage := range stages {
+		outs[i] = stage(in)
+		in = outs[i]
+	}
+
+	go func() {
+		select {
+		case <-done:
+			for _, out := range outs {
+				go emptyReading(out)
+			}
+		case <-finish:
+			return
+		}
+	}()
+
+	go func() {
+	loop:
+		for {
+			select {
+			case res, ok := <-outs[len(outs)-1]:
+				if !ok {
+					close(finish)
+					break loop
+				}
+				out <- res
+			case <-done:
+				break loop
+			}
+		}
+		close(out)
+	}()
+
+	return out
+}
+
+func emptyReading(out Out) {
+	for range out { //nolint:revive
+		// do nothing.
+	}
 }
