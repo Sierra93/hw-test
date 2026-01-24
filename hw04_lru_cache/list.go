1ec4521702e3070b7c72c1bf8366265f118e704d
Anton <timoshenko-ai@ntcees.ru>
2026-01-24 18:26:32 +0300

    Revert "Добавил логику."
    This reverts commit fd42d37f4855241e227c637babdf34ded2e9f48f.

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -2,114 +2,25 @@ package hw04lrucache
 
 type List interface {
 	Len() int
-	Front() *listItem
-	Back() *listItem
-	PushFront(v interface{}) *listItem
-	PushBack(v interface{}) *listItem
-	Remove(i *listItem)
-	MoveToFront(i *listItem)
+	Front() *ListItem
+	Back() *ListItem
+	PushFront(v interface{}) *ListItem
+	PushBack(v interface{}) *ListItem
+	Remove(i *ListItem)
+	MoveToFront(i *ListItem)
 }
 
-// listItem is a list item.
-type listItem struct {
-	Value interface{} // value of list item.
-	Next  *listItem   // next item in list.
-	Prev  *listItem   // previous item in list.
+type ListItem struct {
+	Value interface{}
+	Next  *ListItem
+	Prev  *ListItem
 }
 
-// list is list :).
 type list struct {
-	len  int
-	head *listItem
-	tail *listItem
+	List // Remove me after realization.
+	// Place your code here.
 }
 
-// NewList creates an empty list.
-func NewList() List { //nolint:ireturn
-	return &list{len: 0, head: nil, tail: nil}
-}
-
-// Len returns length of list.
-func (l *list) Len() int {
-	return l.len
-}
-
-// Front returns first item in list.
-func (l *list) Front() *listItem {
-	return l.head
-}
-
-// Back returns last item in list.
-func (l *list) Back() *listItem {
-	return l.tail
-}
-
-// PushFront adds item to head of list.
-func (l *list) PushFront(v interface{}) *listItem {
-	newItem := listItem{Value: v, Next: l.head, Prev: nil}
-
-	if l.tail == nil {
-		l.tail = &newItem
-	} else {
-		l.head.Prev = &newItem
-	}
-	l.head = &newItem
-
-	l.len++
-
-	return l.head
-}
-
-// PushBack adds item to tail of list.
-func (l *list) PushBack(v interface{}) *listItem {
-	newItem := listItem{Value: v, Next: nil, Prev: l.tail}
-
-	if l.head == nil {
-		l.head = &newItem
-	} else {
-		l.tail.Next = &newItem
-	}
-	l.tail = &newItem
-
-	l.len++
-
-	return l.tail
-}
-
-// Remove removes a list item.
-func (l *list) Remove(i *listItem) {
-	if l.head == i {
-		l.head = i.Next
-	} else {
-		i.Prev.Next = i.Next
-	}
-
-	if l.tail == i {
-		l.tail = i.Prev
-	} else {
-		i.Next.Prev = i.Prev
-	}
-
-	l.len--
-}
-
-// MoveToFront moves item to front of list.
-func (l *list) MoveToFront(i *listItem) {
-	if l.head == i {
-		return
-	}
-
-	if l.tail == i {
-		l.tail = i.Prev
-	} else {
-		i.Next.Prev = i.Prev
-	}
-	i.Prev.Next = i.Next
-
-	i.Prev = nil
-	i.Next = l.head
-
-	l.head.Prev = i
-
-	l.head = i
+func NewList() List {
+	return new(list)
 }
