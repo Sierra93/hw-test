15a21587b026544697d57de8c82f9d32242272f5
Anton <timoshenko-ai@ntcees.ru>
2026-01-24 18:20:48 +0300

    !fix

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -6,8 +6,6 @@ import (
 	"io"
 	"os"
 	"path/filepath"
-
-	"github.com/schollz/progressbar/v3"
 )
 
 var (
@@ -66,15 +64,39 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 
 	_, err = source.Seek(offset, io.SeekStart)
 	if err != nil {
-		return fmt.Errorf("failed to read source file: %w", err)
+		return fmt.Errorf("failed to seek in source file: %w", err)
 	}
 
-	bar := progressbar.DefaultBytes(limit)
+	const bufferSize = 32 * 1024 // 32 KB буфер
+	buf := make([]byte, bufferSize)
+	var copied int64 = 0
+	for copied < limit {
+		bytesLeft := limit - copied
+		readSize := bufferSize
+		if bytesLeft < int64(readSize) {
+			readSize = int(bytesLeft)
+		}
 
-	_, err = io.CopyN(io.MultiWriter(destination, bar), source, limit)
-	if err != nil && errors.Is(err, io.EOF) {
-		return fmt.Errorf("failed to copy data from source file to destination file: %w", err)
+		n, err := source.Read(buf[:readSize])
+		if n > 0 {
+			nw, errw := destination.Write(buf[:n])
+			if errw != nil {
+				return fmt.Errorf("ошибка при записи: %w", errw)
+			}
+			copied += int64(nw)
+
+			// каждые 1 Мб или по завершении
+			if copied%(1024*1024) == 0 || copied == limit {
+				fmt.Printf("\rСкопировано %d из %d байт (%.2f%%)", copied, limit, float64(copied)*100/float64(limit))
+			}
+		}
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return fmt.Errorf("ошибка при чтении: %w", err)
+		}
 	}
-
+	fmt.Println("\nКопирование завершено.")
 	return nil
 }
