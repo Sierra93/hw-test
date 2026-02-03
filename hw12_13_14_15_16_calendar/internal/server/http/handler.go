e37fd4e11a26687d615ddb5ee10bed17ac422b1e
Anton <timoshenko-ai@ntcees.ru>
2026-02-03 20:24:33 +0300

    Добавил первую версию.

---
+++ hw12_13_14_15_16_calendar/internal/server/http/handler.go
@@ -0,0 +1,16 @@
+package internalhttp
+
+import "net/http"
+
+type Handler struct {
+	logger Logger
+}
+
+func NewHandler(logger Logger) *Handler {
+	return &Handler{logger: logger}
+}
+
+func (h *Handler) helloHandler(w http.ResponseWriter, r *http.Request) {
+	_ = r
+	w.WriteHeader(http.StatusNoContent)
+}
